30dbf1c8700b009f7da7d107b0674bfe1c1848f5
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-24 01:27:40 +0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Home from './components/Home';
+import Details from './components/Details';
 
-function App() {
-  return (
-    <div className="App">
-      <p>The Weather App is coming.</p>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </Router>
+  </Provider>
+);
 
 export default App;
