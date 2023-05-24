3a15152e0e61eeb92a9098ef114f3cc458a4f268
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-24 23:50:12 +0300

    Modify Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/Filter.css';
 
 const Filter = ({ value, onChange }) => (
-  <select value={value} onChange={onChange}>
-    <option value="">All</option>
-    <option value="cold">Cold (&lt;= 10°C)</option>
-    <option value="moderate">Moderate (10°C - 20°C)</option>
-    <option value="hot">Hot (&gt; 20°C)</option>
-  </select>
+  <div className="filter-container">
+    <p className="filter-label">Filter by Temperature:</p>
+    <select className="filter-select" value={value} onChange={onChange}>
+      <option className="filter-option" value="">All</option>
+      <option className="filter-option" value="cold">Cold (&lt;= 10°C)</option>
+      <option className="filter-option" value="moderate">Moderate (10°C - 20°C)</option>
+      <option className="filter-option" value="hot">Hot (&gt; 20°C)</option>
+    </select>
+  </div>
 );
 
 Filter.propTypes = {
