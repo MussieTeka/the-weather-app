3e3cd701bc78d2d358db4cb3abbbcb2b0f32afc2
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-27 15:05:26 +0300

    Modify CityList.js

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../style/CityList.css';
+import right from '../images/right.png';
 
 const CityList = ({ cities, getWeatherImage }) => (
   <div className="city-list-container">
     {cities.map((city) => (
       <div key={city.id} className="city-box">
-        <img src={getWeatherImage(city.main.temp)} alt="Weather" className="weather-image" />
+        <div className="city-top">
+          <img src={getWeatherImage(city.main.temp)} alt="Weather" className="weather-image" />
+          <img src={right} alt="Right Arrow" className="right-arrow" />
+        </div>
         <Link to={`/details/${city.id}`} className="city-link">
           {city.name}
         </Link>
