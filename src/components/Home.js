39085b98ddabd4f960ee5d96ae055aede887a1a8
Mussie Teka <mussiekahasay12@gmail.com>
2023-05-27 08:38:58 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import AddCity from './AddCity';
 import CityList from './CityList';
 import Filter from './Filter';
+import cold from '../images/cold.png';
+import hot from '../images/hot.png';
+import moderate from '../images/moderate.png';
+import season from '../images/season.png';
 import '../style/Home.css';
 
 const API_KEY = 'f4dcc1ff0c9c9801258e79bae8709b84';
@@ -11,24 +15,19 @@ const Home = () => {
   const dispatch = useDispatch();
   const cities = useSelector((state) => state.cities);
   const [filter, setFilter] = useState('');
-  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
-        const cityIds = [524901, 703448, 1283240, 2643743, 3169070, 1835848, 1701668, 745044];
+        const cityIds = [3421319, 5880054, 2122311, 6185377, 2643743,
+          1850147, 6173331, 5391959, 292223, 360630, 1609350, 2147714];
         const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/group?id=${cityIds.join(',')}&units=metric&appid=${API_KEY}`,
+          `http://api.openweathermap.org/data/2.5/group?id=${cityIds.join(',')}&units=metric&appid=${API_KEY}`,
         );
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch weather data');
-        }
-
         const data = await response.json();
         dispatch({ type: 'SET_CITIES', payload: data.list });
       } catch (error) {
-        setError('Error fetching weather data');
+        throw new Error('Error fetching weather data:', error);
       }
     };
 
@@ -43,6 +42,19 @@ const Home = () => {
     setFilter(e.target.value);
   };
 
+  const getWeatherImage = (temperature) => {
+    if (temperature <= 10) {
+      return cold;
+    }
+    if (temperature > 10 && temperature <= 20) {
+      return moderate;
+    }
+    if (temperature > 20) {
+      return hot;
+    }
+    return season;
+  };
+
   const filteredCities = cities.filter((city) => {
     if (filter === '') return true;
     if (filter === 'cold') return city.main.temp <= 10;
@@ -53,8 +65,9 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h1>Weather App</h1>
-      {error && <div className="error-message">{error}</div>}
+      <div className="title-container">
+        <p className="title">Weather App</p>
+      </div>
       <div className="mobile-layout">
         <div className="top-row">
           <div className="box">
@@ -64,8 +77,9 @@ const Home = () => {
             <Filter value={filter} onChange={handleFilterChange} className="filter-select" />
           </div>
         </div>
+        <p className="city-text">CITIES AND THIER WEATHER.</p>
         <div className="box">
-          <CityList cities={filteredCities} className="city-list" />
+          <CityList cities={filteredCities} getWeatherImage={getWeatherImage} className="city-list" />
         </div>
       </div>
     </div>
